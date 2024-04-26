9248800eeca2c44303b422c60ca90e5c7a1a398e
Lana Zhang <zhangblue@gmail.com>
2024-04-25 21:08:44 -0400

    Update deploy-cloudshell.sh with default opensearch config

--- deploy-cloudshell.sh
+++ deploy-cloudshell.sh
@@ -10,6 +10,9 @@ pip install -r requirements.txt
 # Bootstrap CDK - this step will launch a CloudFormation stack to provision the CDK package, which will take ~2 minutes.
 cdk bootstrap aws://${CDK_DEFAULT_ACCOUNT}/${CDK_DEFAULT_REGION}
 
+# If variables are empty or unset, set it to the default value
+: "${CDK_INPUT_OPENSEARCH_CONFIG:='Dev'}"
+
 echo "cdk deploy --parameters inputUserEmails=${CDK_INPUT_USER_EMAILS} -c env=${CDK_INPUT_OPENSEARCH_CONFIG}  --requires-approval never  --all"
 # Deploy CDK package - this step will launch one CloudFormation stack with three nested stacks for different sub-systems.
 cdk deploy --parameters inputUserEmails=${CDK_INPUT_USER_EMAILS} -c env=${CDK_INPUT_OPENSEARCH_CONFIG}  --requires-approval never  --all
